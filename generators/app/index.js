06de55634473f274603d73a15ccac6732408b617
Peng <1936478563@qq.com>
2022-10-15 21:14:42 +0800

    Fix：修复项目名字存在大写字母问题

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,46 +14,47 @@ module.exports = class extends Generator {
       },
       {
         type: "input",
-        name:"author",
-        message: "Please enter the name of the project author:(Default GinPang)",
-        default: "GinPang"
+        name: "author",
+        message:
+          "Please enter the name of the project author:(Default GinPang)",
+        default: "GinPang",
       },
       {
         type: "list",
         name: "license",
         message: "Please select a license for the project :",
-        choices:["MIT","Apache"]
+        choices: ["MIT", "Apache"],
       },
       {
-        type:"input",
-        name:"repo",
+        type: "input",
+        name: "repo",
         message: "Please enter repository address:",
-        default: ""
-      }
+        default: "",
+      },
     ]);
-    this.porps.name = props.name;
+    this.porps.name = props.name.toLowerCase();
     this.porps.author = props.author;
-    this.porps.license=props.license;
-    this.porps.repo=props.repo;
+    this.porps.license = props.license;
+    this.porps.repo = props.repo;
   }
   async configuring() {
     this.log("Starting copy file ...");
-    try{
-        await fs.copy(`${this.sourceRoot()}`,`${this.destinationRoot()}`)
-    }catch(e){
-        this.log(e)
+    try {
+      await fs.copy(`${this.sourceRoot()}`, `${this.destinationRoot()}`);
+    } catch (e) {
+      this.log(e);
     }
   }
-  writing(){
-    this.log("Writing package.json file ...")
-    const pkg=require("./package-template.json")
-    pkg.name=this.porps.name
-    pkg.author=this.porps.author
-    pkg.license=this.porps.license
-    pkg.repository=this.porps.repo
-    this.fs.writeJSON(this.destinationPath("package.json"),pkg)
+  writing() {
+    this.log("Writing package.json file ...");
+    const pkg = require("./package-template.json");
+    pkg.name = this.porps.name;
+    pkg.author = this.porps.author;
+    pkg.license = this.porps.license;
+    pkg.repository = this.porps.repo;
+    this.fs.writeJSON(this.destinationPath("package.json"), pkg);
   }
-  ending(){
-    this.log("Bey!!!")
+  ending() {
+    this.log("Bey!!!");
   }
 };
